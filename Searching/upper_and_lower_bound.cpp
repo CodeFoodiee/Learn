ca6cb7652c493afefc73bd5b9a388b48d9b883a7
DEEPESH SINGH <sanemi@Sanemi-M4.local>
2026-03-13 23:50:09 +0530

    This is a code

---
+++ Searching/upper_and_lower_bound.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Bound{
+public:
+    int lowerbound(vector<int> v , int x){
+        int n = v.size();
+        int low = 0 , high = n-1;
+        int result = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(mid >= x){
+                result = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+    
+    int upperbound(vector<int> v , int x){
+        int n = v.size();
+        int low = 0 , high = n-1;
+        int result = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(mid > x){
+                result = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+     vector<int> v;
+    int x;
+    int n;
+    cin>>n;
+    v.resize(n);
+
+    for(int i = 0 ; i < v.size() ; i++){
+        cin>>v[i];
+    }
+    
+    cin>>x;
+
+    Bound b1;
+    int lowerB = b1.lowerbound(v,x);
+    int upperB = b1.upperbound(v,x);
+
+    cout<<"Lower bound : "<<lowerB<<endl<<"Upper bound : "<<upperB<<endl;
+    return 0;
+}
